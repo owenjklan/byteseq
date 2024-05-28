49e28ca373316e3fa579deb3bc03c435d944982a
Owen Klan <owen.j.klan@gmail.com>
2024-05-28 10:23:42 +1000

    Add documentation for package. Empty README for now.

--- byteseq.go
+++ byteseq.go
@@ -1,3 +1,8 @@
+// Package byteseq implements a randomised byte sequence.
+//
+// Returns values in the byte range of 0-255, but returns each
+// value only once. Will return an error if an attempt is made
+// to get a value from an 'exhausted' sequence.
 package byteseq
 
 import (
@@ -5,11 +10,17 @@ import (
 	"math/rand"
 )
 
+// A RandomByteSeq returns byte values randomly and only once each.
+// Byte values can be marked as already having been consumed when
+// creating a new struct.
 type RandomByteSeq struct {
 	consumedBytes   [32]byte // Bitmap for each of 256 possible values
 	remainingValues int
 }
 
+// NewRandomSeq returns a new RandomByteSeq. consumedBytes is a slice of
+// optional values to exclude from the sequence. Internally, they are considered
+// as having already been "consumed".
 func NewRandomSeq(consumedBytes []byte) *RandomByteSeq {
 	seq := &RandomByteSeq{}
 
@@ -24,7 +35,9 @@ func NewRandomSeq(consumedBytes []byte) *RandomByteSeq {
 	return seq
 }
 
+// determine if a given byte value is marked as consumed in the internal bitmap structures
 func (r *RandomByteSeq) valueHasBeenConsumed(b byte) bool {
+	// determine if a given byte value is marked as consumed in the internal bitmap structures
 	// Get the correct byte of our bitmap from top 5 bits
 	byteIndex := b & 0xF8 >> 3
 	testByte := r.consumedBytes[byteIndex]
@@ -36,6 +49,7 @@ func (r *RandomByteSeq) valueHasBeenConsumed(b byte) bool {
 }
 
 func (r *RandomByteSeq) consumeByte(b byte) {
+	// marks a given byte value as having been consumed in the internal bitmap structures.
 	byteIndex := b & 0xF8 >> 3
 	bitMask := byte(1 << (b & 0x07))
 
@@ -43,10 +57,16 @@ func (r *RandomByteSeq) consumeByte(b byte) {
 	r.remainingValues--
 }
 
+// HasMore is intended to check if the RandomByteSeq has any more values
+// that it can return.
 func (r *RandomByteSeq) HasMore() bool {
 	return r.remainingValues > 0
 }
 
+// NextValue returns either the next random byte value that hasn't been
+// previously returned, or will return an error if an attempt is made
+// to get a value from an exhausted sequence. The HasMore function
+// can be used to avoid this.
 func (r *RandomByteSeq) NextValue() (byte, error) {
 	// Are there any more values available?
 	if r.remainingValues == 0 {
